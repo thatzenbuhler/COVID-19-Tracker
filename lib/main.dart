0d6745f94e05f381de76cea38c7aeeee1d6da814
thatzenbuhler <t.hatzenbuhler@gmail.com>
2020-04-03 14:46:04 -0700

    Working webview and home button

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+// COVID-19 Map Tracker
+// Tyler Hatzenbuhler
+// Data from Johns Hopkins University
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,16 +34,30 @@ class _MyHomePageState extends State<MyHomePage> {
   final String pagelink =
       "https://www.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61";
 
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
           backgroundColor: Colors.blueGrey[900],
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.home), onPressed: () {
+              if (_controller.isCompleted) {
+                _controller.future.then((result) {
+                  result.loadUrl(pagelink);
+                });
+              }
+            })
+          ],
         ),
         body: WebView(
           initialUrl: pagelink,
           javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
         ));
   }
 }
